277886cbb46017ccab6a17a823e3fe631d0bbd24
Luka Zakrajšek <luka@bancek.net>
2020-09-07 22:44:49 +0200

    Move healthServiceMock

--- pkg/lockproxy/helpers_test.go
+++ pkg/lockproxy/helpers_test.go
@@ -1,14 +1,11 @@
 package lockproxy_test
 
 import (
-	"context"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/onsi/ginkgo"
 	"github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/mock"
-	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func NewLogger() *logrus.Logger {
@@ -31,18 +28,4 @@ func NewLoggerEntry() *logrus.Entry {
 // Rand returns random 8 characters
 func Rand() string {
 	return uuid.New().String()[:8]
-}
-
-type healthServiceMock struct {
-	mock.Mock
-}
-
-func (m *healthServiceMock) Check(ctx context.Context, req *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
-	args := m.Called()
-	return args.Get(0).(*grpc_health_v1.HealthCheckResponse), args.Error(1)
-}
-
-func (m *healthServiceMock) Watch(req *grpc_health_v1.HealthCheckRequest, w grpc_health_v1.Health_WatchServer) error {
-	args := m.Called()
-	return args.Error(0)
 }
